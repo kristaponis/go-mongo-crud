b1a1439dbdbd47ac634eaf4c54ecd12f37fc9e89
kristaponis <a.kristaponis@icloud.com>
2021-01-18 12:46:31 +0200

    Added connection to MongoDB server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kristaponis/go-mongo-crud
 
 go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.4 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 func main() {
-	
+	// connect to the MongoDB server, which is running local
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// list all database names in MongoDB server (check for connection)
+	dbs, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dbs)
 }
